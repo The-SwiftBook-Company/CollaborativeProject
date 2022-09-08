a9288d075396fce680e2718c764ec54e857d3cf3
PX001-hub <54709568+PX001-hub@users.noreply.github.com>
2022-09-08 15:55:27 +0300

    Initial Commit

---
+++ Members Of The Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Members Of The Project
+//
+//  Created by P Z on 08/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
