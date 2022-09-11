47c8ec144516fd4f2d2ccecf43027b4fb4c32398
Roman Khilchenko <extrimelife@hotmail.com>
2022-09-11 14:30:54 +0300

    Новый репозиторй

---
+++ VillagioRealtyApp/ViewControllers/CustomTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CustomTableViewCell.swift
+//  VillagioRealtyApp
+//
+//  Created by roman Khilchenko on 06.09.2022.
+//
+
+import UIKit
+
+final class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var houseImage: UIImageView!
+    @IBOutlet weak var titleHouseLabel: UILabel!
+    @IBOutlet weak var priceHouseLabel: UILabel!
+    @IBOutlet weak var placeHouseLabel: UILabel!
+    
+    
+}
+    
+    
+    
